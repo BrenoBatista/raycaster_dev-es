97db60103a2b99b4bbbc24fda78edfe1d666b668
Breno Nunes Batista <brenonunesbatista@hotmail.com>
2025-08-12 22:05:29 -0300

    map drawn

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 from settings import *
+from map import Map
 
 screen =  pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+map = Map()
 
 while True:
     for event in pygame.event.get():
@@ -11,5 +14,7 @@ while True:
 
 
     screen.fill((0, 0, 0))
+    
+    map.render(screen)
 
     pygame.display.update()
